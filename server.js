98e93873a35e64d8dd545f9ffa4b725e1ef3f834
Yurii <fanfjrfj589@gmail.com>
2023-09-18 18:39:08 +0200

    finish fix bags with login and register, also add validation

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+const db = mongoose.connection;
 
 console.log(process.env.DB_URL);
 mongoose.connect(process.env.DB_URL, {
@@ -44,41 +45,58 @@ app.use(cors());
 app.get("/", (req, resp) => {
 
 	resp.send("App is Working");
-});
-
-app.post('/register', async (req, resp) => {
+});	
+	
+app.post('/register', async (req, res) => {
 	try {
-	  const user = new User(req.body);
-	  let result = await user.save();
-	  result = result.toObject();
-	  if (result) {
-		delete result.password;
-		console.log(result);
-		console.log(result._id);
+	  const { login, password, savePassword } = req.body;
   
-		// Send a success response
-		return resp.send(result);
+	  const existingUser = await User.findOne({ login });
+  
+	  if (existingUser) {
+		return res.status(400).json({ error: 'User with this login already exists' });
+	  }
+  
+	  const user = new User({ login, password, savePassword });
+	  let result = await user.save();
+  
+	  result = result.toObject();
+  
+	  if (result) {
+		// delete result.password; // Remove the password from the response
+		console.log(result);
+		return res.send(result);
 	  } else {
 		console.log('User already registered');
-		// Send an error response if needed
-		return resp.status(400).send('User already registered');
+		return res.status(400).send('User already registered');
 	  }
 	} catch (e) {
 	  console.error('Error:', e);
-	  // Send an error response
-	  return resp.status(500).send('Something Went Wrong');
+	  return res.status(500).send('Something Went Wrong');
 	}
   });
+
+ app.get('/get-user', async (req, res) => {
+  try {
+    const { login, password } = req.query;
+
+    const user = await User.findOne({ login, password });
+
+    if (user) {
+      const sanitizedUser = user.toObject();
+    //   delete sanitizedUser.password;
+      res.json(sanitizedUser);
+    } else {
+      console.log('User not found or incorrect password');
+      res.status(404).json({ error: 'User not found or incorrect password' });
+    }
+  } catch (error) {
+    console.log(`Error: ${error}`);
+    res.status(500).json({ error: 'Error getting user' });
+  }
+});
+
   
-  app.get('/get-users', async (req, res) => {
-	try {
-	  const users = await User.find({});
-	  res.json(users);
-	} catch (error) {
-	  console.log(`Error: ${error}`);
-	  res.status(500).json({ error: 'Error getting users' });
-	}
-  });
   
 
 app.listen(5000);
